48ddbb14246966cc10db674f4240a184503571bc
Irene Vega Lago <ivega@gamma19.aulas.gsyc.urjc.es>
2017-10-23 15:39:56 +0200

    Parte 6

--- client.py
+++ client.py
@@ -3,27 +3,26 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-import sys
 import socket
+import sys
 
 if len(sys.argv) <4:
-    sys.exit(' Use:python3 client.py ip puerto linea')
+    sys.exit(' Use:python3 client.py ip port linea')
     
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1] #string
 PORT = sys.argv[2]
-LINE = sys.argv[3:]
+LINE = sys.argv[4:]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 #parametros:internet, IP, UDP
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, int(PORT)))
     print("Enviando:", ' '.join(LINE))
-    my_socket.send(bytes(' '.join(LINE), 'utf-8') + b'\r\n')
-    #convierto para poder mandar
-    #2formas pasar bytes
-    data = my_socket.recv(1024)
-    #Se queda esperando si no llega
+    if sys.argv[3] == 'register':
+        my_socket.send(bytes('REGISTER sip:'+' '.join(LINE)+' SIP/2.0\r\n\r\n',      'utf-8') + b'\r\n')
+    #convierto para poder mandar - 2formas pasar bytes
+        data = my_socket.recv(1024) #Se queda esperando si no llega
     print('Recibido -- ', data.decode('utf-8')) #vuelvo a convertir utf8
 
 print("Socket terminado.")
