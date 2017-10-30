97f831f0e5e3561394e7a110d8764d41964bfd34
Irene Vega Lago <ivega@gamma07.aulas.gsyc.urjc.es>
2017-10-30 16:02:47 +0100

    Fin

--- client.py
+++ client.py
@@ -6,16 +6,14 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-if len(sys.argv) < 5:
+if len(sys.argv) != 6:
     sys.exit(' Use:client.py ip puerto register sip_address expires_value')
 
-# Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 LINE = sys.argv[4]
 EXPIRE = sys.argv[5]
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-# parametros:internet, IP, UDP
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
@@ -23,5 +21,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.send(bytes('REGISTER sip:' + LINE + ' SIP/2.0\r\nExpires: ' +
                              EXPIRE+'\r\n\r\n', 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+        print('Recibido -- ', data.decode('utf-8'))
 print("Socket terminado.")
